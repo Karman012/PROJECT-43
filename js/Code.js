7e85189584f06ce73379ccc5d412c9456ef33ea7
Karman012 <77972806+Karman012@users.noreply.github.com>
2021-06-18 20:24:11 +0800

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,17 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill("white");
+    textSize (15);
+    text ("R I A B A V E L", 100, 50);
+    
+    fill("white");
+    textSize (15);
+    text ("F C U N T N I O", 700, 150);
+    
+    fill("white");
+    textSize (15);
+    text ("A D B A T E S A", 100, 250);
+}
